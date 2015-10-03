bf017d8efde53440da9de5bbc5865af9fc601537
brian.price <brian.price@rackspace.com>
2015-10-03 14:10:38 -0500

    Documenting when we are using what secretkey

--- sugarcoat/api/base.py
+++ sugarcoat/api/base.py
@@ -4,4 +4,11 @@ import random
 import os
 
 app = flask.Flask(__name__)
-app.secret_key = os.environ.get('sugarcoat_secret') or ''.join(random.sample(string.ascii_letters, 24))
+ENVIRON_SECRETKEY_NAME = 'sugarcoat_secret'
+if ENVIRON_SECRETKEY_NAME in os.environ:
+    print("Using environment {0} for secret_key".format(ENVIRON_SECRETKEY_NAME))
+    app.secret_key = os.environ[ENVIRON_SECRETKEY_NAME]
+else:
+    print("Randomly generating secret key")
+    app.secret_key = ''.join(random.sample(string.ascii_letters, 24))
+app.jinja_options['extensions'].append('jinja2.ext.do')
