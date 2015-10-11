66044b93e46a5b3c3bf700d6e9213d747bb97c76
brian.price <brian.price@rackspace.com>
2015-10-11 12:23:05 -0500

    Adding loggin information.

--- sugarcoat/api/base.py
+++ sugarcoat/api/base.py
@@ -3,6 +3,7 @@ import string
 import random
 import os
 from sugarcoat.base import SUGARCOAT_RESTFUL_KEY
+from werkzeug.routing import Rule
 
 app = flask.Flask(__name__)
 ENVIRON_SECRETKEY_NAME = 'sugarcoat_secret'
@@ -16,29 +17,48 @@ app.jinja_options['extensions'].append('jinja2.ext.do')
 
 @app.errorhandler(404)
 def page_not_found(e):
-    result = cake_lie_view()
+    result = cake_lie_view(404)
     return result, 404
 
 @app.route('/')
 def index():
     return flask.Response(flask.render_template('main_index.html'))
 
-@app.route('/cake_is_a_lie.html')
-def cake_lie_view():
+app.url_map.add(Rule('/cake_is_a_lie.html', endpoint='cake_lie_view'))
+
+@app.endpoint('/cake_lie_view', )
+def cake_lie_view(response_code=200):
+    should_log = True
+    if should_log:
+        app.logger.info("Request {1} {0} was provided".format(flask.request.url, flask.request.method))
+        app.logger.info("Headers =====\n{0}\n====".format(flask.request.headers))
+
+        if flask.request.args:
+            app.logger.info("Args =====\n{0}\n====".format(flask.request.args))
+        if flask.request.data:
+            app.logger.info("Data =====\n{0}\n====".format(flask.request.data))
     restful_result = {"random":
-             {"status": 404,
+             {"status": response_code,
               "message": "Not sure how you got here.",
               SUGARCOAT_RESTFUL_KEY:{
-              "links": [
-                  {"rel": "mouse breaker", "href": 'http://orteil.dashnet.org/cookieclicker/'},
-                  {"rel": "browser mmo", "href": 'http://agar.io/'},
-                  {"rel": "selfless or selfish", "href": 'http://cursors.io/'},
-                  {"rel": "RPG...I guess", "href": 'http://nekogames.jp/g.html?GID=gm0000001351&OTP&SID=9003216&r=1333941573&gid=PRM'},
-                  {"rel": "coding music", "href": 'https://www.youtube.com/watch?v=hGlyFc79BUE'},
-                  {"rel": "repo", "href": 'https://github.com/badcure/sugarcoat'},
-                  {"rel": "bookmark", "href": 'https://sugarcoat.in'},
-                  {"rel": "self", "href": 'https://sugarcoat.in/cake_is_a_lie.html'}
-              ]
-              }}
+                  "links": [
+                      {"rel": "mouse breaker", "href": 'http://orteil.dashnet.org/cookieclicker/'},
+                      {"rel": "browser mmo", "href": 'http://agar.io/'},
+                      {"rel": "selfless or selfish", "href": 'http://cursors.io/'},
+                      {"rel": "RPG...I guess", "href": 'http://nekogames.jp/g.html?GID=gm0000001351&OTP&SID=9003216&r=1333941573&gid=PRM'},
+                      {"rel": "coding music", "href": 'https://www.youtube.com/watch?v=hGlyFc79BUE'},
+                      {"rel": "repo", "href": 'https://github.com/badcure/sugarcoat'},
+                      {"rel": "bookmark", "href": 'https://sugarcoat.in'},
+                      {"rel": "self", "href": flask.url_for('cake_lie_view',_external=True)},
+                      {"rel": "request_url", "href": flask.request.url},
+                  ],
+                  'method': str(flask.request.method),
+                  'url': str(flask.request.url),
+                  'args': str(dict(flask.request.args)),
+                  'data': str(flask.request.data),
+                  'headers': str(flask.request.headers),
+                  'info_logged': should_log
+              }
+              }
          }
     return flask.jsonify(restful_result)
